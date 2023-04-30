a27e735c3d1de5e9879d9e0d87ce89b3275d18db
edeColli <ednesio.colli@gmail.com>
2023-04-30 09:58:07 -0300

    Criando classe ClienteBuilder

--- src/main/java/com.ultimaschool.java/Main.java
+++ src/main/java/com.ultimaschool.java/Main.java
@@ -1,6 +1,7 @@
 package com.ultimaschool.java;
 
 import com.ultimaschool.java.clientes.Cliente;
+import com.ultimaschool.java.clientes.ClienteBuilder;
 
 public class Main {
 
@@ -15,5 +16,14 @@ public class Main {
                 "09/06/1981", 'M', "rogerio@mail.com", "Av. Flores",
                 "(83) 8888-8888");
         System.out.println(clienteM.toString());
+
+        ClienteBuilder clienteFCompleto = new ClienteBuilder().comIdentificacao("Joana", "Maria",
+                "Rocha","123.456.789-00","10/01/1993", 'F').comContatos(
+                        "joana@mail.com", "Av. Brasil","(83) 9999-9999");
+        System.out.println(clienteFCompleto.toString());
+
+        ClienteBuilder clienteFContato = new ClienteBuilder().comContatos("joana@mail.com", "Av. Brasil",
+                "(83) 9999-9999");
+        System.out.println(clienteFContato.toStringContatos());
     }
 }
